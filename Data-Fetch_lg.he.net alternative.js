4e69025adcebdbb9062597f60877d89d24013ae1
T-Yobart <70069550+T-Yobart@users.noreply.github.com>
2020-08-22 23:38:57 +0200

    added version of script couting dupes
    added alternative script referenced in first iteration of readme

---
+++ Data-Fetch_lg.he.net alternative.js
@@ -0,0 +1,95 @@
+///////////////////////////
+/// inclusion of Jquery ///
+///////////////////////////
+var script = document.createElement('script'); 
+ 
+script.src = '//code.jquery.com/jquery-1.11.0.min.js'; 
+document.getElementsByTagName('head')[0].appendChild(script); 
+
+///////////////////////////
+///    variables init   ///
+///////////////////////////
+
+//outputs
+var neighbors= []; // ASN of all found BGP neighbors
+var i =0;   // number of sent requests
+
+//script variables
+var table;  //Output of Latest POST request
+var doc;    //HTML Document parsed from table
+var parser = new DOMParser(); //initialization of string to HTML parser
+var routers =$("#lg_routers").find("ul ul li input"); //fetching all input nodes relating routers
+
+///////////////////////////
+///    sub functions    ///
+///////////////////////////
+
+function sleep(ms) {
+    //function used to emulate a delay
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+function sort (data){
+    //parsing of the received POST request to fetch the DOM
+    //and sorting the output into output arrays
+
+    table = data;
+    doc = parser.parseFromString(table, "text/html");
+
+    $(doc).find(".tablesorter tbody tr ").each(function(){
+        var asn = $(this).find("td:eq(1)").text();
+        var found = false;
+        $(neighbors).each(function(){
+            if(this[0]==asn){
+                this[1]=this[1]+1;
+                found=true;
+            };
+        })
+        if(!found){
+            neighbors.push([asn,1]);
+        };
+    });
+};
+
+function send (){  
+    //used to asynchronously send a request to the server
+
+    var form = $("form");
+    var url = form.attr('action');
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(), // serializes the form's elements.
+        success: function(data){
+            sort(data); // call to sorting function
+        }
+    })
+};
+
+///////////////////////////
+///        main         ///
+///////////////////////////
+$(document).ready(async function(){
+
+    //selection of the command to be used
+    $("#command_bgpsummary6").prop("checked", true);
+    
+    //deselecting all routers
+    $(routers).each(function(){
+        $(this).prop( "checked", false );
+    });
+    
+    //main loop
+    for (var router; i < routers.length; i++) {
+        //selecting router sending request and waiting for
+        //the cooldown to en in order to continue iteration
+
+        router = routers[i];
+        $(router).prop( "checked", true );
+        send(); //call to request sending function
+        $(router).prop( "checked", false );
+        await sleep(60000);
+    };
+  
+}); 
